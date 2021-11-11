fd0eccf78592b9a01f0ce033bd7fc68efeab0008
Danylo Kuzebnyi <danilych77767@gmail.com>
2021-11-11 23:31:35 +0200

    Correction the mistakes

--- hw1/src/main/java/org/geekhub/danylo/Number.java
+++ hw1/src/main/java/org/geekhub/danylo/Number.java
@@ -5,60 +5,47 @@ import java.util.Scanner;
 public class Number {
 
     public static void main(String[] args) {
-
-        String number, regex;
-        boolean flag;
-        regex = "\\d+";
+        String phoneNumber;
+        String regex1, regex2, regex3;
+        boolean validPhone;
+        regex1 = "^\\+38\\(?03[9]\\)?\\d{7}|^\\+38\\(?05[0]\\)?\\d{7}|^\\+38\\(?06[3678]\\)?\\d{7}|^\\+38\\(?07[3]\\)?\\d{7}|^\\+38\\(?09[1-9]\\)?\\d{7}";
+        regex2 = "^8\\(?03[9]\\)?\\d{7}|^8\\(?05[0]\\)?\\d{7}|^8\\(?06[3678]\\)?\\d{7}|^8\\(?07[3]\\)?\\d{7}|^8\\(?09[1-9]\\)?\\d{7}";
+        regex3 = "^\\(?03[9]\\)?\\d{7}|^\\(?05[0]\\)?\\d{7}|^\\(?06[3678]\\)?\\d{7}|^\\(?07[3]\\)?\\d{7}|^\\(?09[1-9]\\)?\\d{7}";
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("Input your telephone number: ");
-            number = in.nextLine();
-            flag = ( ( number.startsWith("+380") && number.length() == 13 && number.substring(1).matches(regex) )
-                    || ( number.startsWith("0") && number.length() == 10 && number.matches(regex) )
-                    || ( number.startsWith("8") && number.length() == 11 && number.matches(regex) ) );
-        } while (!flag);
+            phoneNumber = in.nextLine();
+            validPhone = phoneNumber.matches(regex1)||phoneNumber.matches(regex2)||phoneNumber.matches(regex3);
+        } while (!validPhone);
+        System.out.println("The phone number is correct!");
         in.close();
 
-        if ( ( number.startsWith("+380") && number.length() == 13 && number.substring(1).matches(regex) )
-                || ( number.startsWith("0") && number.length() == 10 && number.matches(regex) )
-                || ( number.startsWith("8") && number.length() == 11 && number.matches(regex) ) ) {
-
-            System.out.println("The phone number is correct!");
-            int sum = 0;
-            for (int i = 0; i < number.length(); i++) {
-                if (Character.isDigit(number.charAt(i)))
-                    sum += Character.getNumericValue(number.charAt(i));
+        long sum;
+        int n = 0;
+        do {
+            sum = 0;
+            for (int i = 0; i < phoneNumber.length(); i++) {
+                if (Character.isDigit(phoneNumber.charAt(i)))
+                    sum += Character.getNumericValue(phoneNumber.charAt(i));
             }
-            System.out.println("1st round of calculation, sum is: " + sum);
+            Long phoneNumberLong = Long.parseLong(phoneNumber);
+            phoneNumberLong = sum;
+            phoneNumber = phoneNumberLong.toString();
+            n++;
+            System.out.println(n + " round of calculation, sum is: " + sum);
+        } while (sum > 9);
 
-            int sum2 = 0;
-            while (sum > 9) {
-                sum2 = 0;
-                String str_sum = String.valueOf(sum);
-                for (int i = 0; i < str_sum.length(); i++) {
-                    sum2 += Character.getNumericValue(str_sum.charAt(i));
-                }
-                sum = sum2;
-                System.out.println("2nd round of calculation, sum is: " + sum2);
-            }
-            
-            switch (sum2) {
-                case 1:
-                    System.out.println("Final result is: One");
-                    break;
-                case 2:
-                    System.out.println("Final result is: Two");
-                    break;
-                case 3:
-                    System.out.println("Final result is: Three");
-                    break;
-                case 4:
-                    System.out.println("Final result is: Four");
-                    break;
-                default:
-                    System.out.println("Final result is: " + sum2);
-            }
-
+        if (sum == 1) {
+            System.out.println("Final result is: One");
+        } else if (sum == 2) {
+            System.out.println("Final result is: Two");
+        } else if (sum == 3) {
+            System.out.println("Final result is: Three");
+        } else if (sum == 4) {
+            System.out.println("Final result is: Four");
+        } else {
+            System.out.println("Final result is: " + sum);
         }
     }
 }
+
