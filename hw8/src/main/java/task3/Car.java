a39dda74c66a6372e2c94b0b9ab94f704aa9c573
Данил Кузебный <danilych77767@gmail.com>
2021-12-26 09:55:55 +0000

    Car.java edited online with Bitbucket

--- hw8/src/main/java/task3/Car.java
+++ hw8/src/main/java/task3/Car.java
@@ -3,14 +3,14 @@ package task3;
 import task1.Ignore;
 
 public class Car {
+    @Ignore
+    private final int power = 250;
     private String model;
     private String type;
     private String color;
     private int maxSpeed;
     private int passengerCapacity;
-    @Ignore
-    private final int power = 250;
-
+    
     public Car(String model, String type, String color, int maxSpeed, int passengerCapacity) {
         this.model = model;
         this.type = type;
