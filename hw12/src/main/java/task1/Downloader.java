ec7228ce474b7e4f134b75949555a00b3bad79de
Данил Кузебный <danilych77767@gmail.com>
2022-02-06 15:22:25 +0000

    Downloader.java renamed some fields

--- hw12/src/main/java/task1/Downloader.java
+++ hw12/src/main/java/task1/Downloader.java
@@ -7,12 +7,13 @@ import java.util.List;
 public class Downloader {
 
     public void downloadImages(List<String> linksOf200BestPhotos, int start, int end) throws IOException {
-        for (int i = start; i < end;) {
+        for (int i = start; i < end; i++) {
             String link = linksOf200BestPhotos.get(i);
             URL url = new URL(link);
-            i++;
+            int imageNumber = i + 1;
+            String fileName = "hw12/src/main/resources/images/" + imageNumber + ".jpg";
             try (InputStream in = new BufferedInputStream(url.openStream());
-                 FileOutputStream fos = new FileOutputStream("hw12/src/main/resources/images/" + i + ".jpg");
+                 FileOutputStream fos = new FileOutputStream(fileName);
                  OutputStream out = new BufferedOutputStream(fos)) {
                 for (int j; (j = in.read()) != -1;) {
                     out.write(j);
